e02be09c29440b803b9a4c17e0f4c7cbd6d65796
chang3106 <chang31063992@gmail.com>
2016-01-10 17:36:15 +0800

    update db.js to mongolab

--- api/db.js
+++ api/db.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 
 //mongodb connection
-var mongolab_db = '';
+var mongolab_db = 'mongodb://admin:admin@ds039165.mongolab.com:39165/canvas-dev';
 //var mongolab_db = 'mongodb://admin:admin@ds043971.mongolab.com:43971/teamboard-dev-heroku';
 var localhost_db = 'mongodb://localhost:27017/canvas-dev';
 
-mongoose.connect(localhost_db, function(err){
+mongoose.connect(mongolab_db, function(err){
 	if(err){
 		console.log(err);
 	} else{
