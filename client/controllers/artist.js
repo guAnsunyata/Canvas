7dc34b2c6abfb1a9340a754928e05be7793df2ed
guAnsunyata <guansunyata@gmail.com>
2016-01-13 20:09:40 +0800

    demo 0.1 update artist

--- client/controllers/artist.js
+++ client/controllers/artist.js
@@ -3,39 +3,13 @@ var myApp = angular.module('myApp');
 myApp.controller('ArtistController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
 	console.log('ArtistController loaded...');
 	var id = $routeParams.id;
-	var data = [{
-		_id: 1,
-	    name: 'consuming-fire',
-	    artist: 'Jake Weidmann',
-	    image: 'images/pictureExample/Painting/Jake Weidmann/consuming-fire.jpg'
-	},{
-		_id: 2,
-	    name: 'consuming-fire',
-	    artist: 'Jake Weidmann',
-	    image: 'images/pictureExample/Calligraphy/Jake Weidmann/STEWARD.jpg'
-	},{
-		_id: 3,
-	    name: 'consuming-fire',
-	    artist: 'Jake Weidmann',
-	    image: 'images/pictureExample/Calligraphy/Jake Weidmann/THE SOJOURNER\'S ROSE.jpg'
-	},{
-		_id: 4,
-	    name: 'consuming-fire',
-	    artist: 'Jake Weidmann',
-	    image: 'images/pictureExample/Painting/Jake Weidmann/Indivisible.jpg'
-	},{
-		_id: 5,
-	    name: 'consuming-fire',
-	    artist: 'Jake Weidmann',
-	    image: 'images/pictureExample/Painting/Jake Weidmann/Humming-A-Love-Song.jpg'
-	},{
-		_id: 6,
-	    name: 'consuming-fire',
-	    artist: 'Jake Weidmann',
-	    image: 'images/pictureExample/Painting/Jake Weidmann/Transformation.jpg'
-	}];
-	$scope.works = data;
-	$scope.artist = 'Jake Weidmann';
+	
+	$http.post('/api/findOneArtist',{user_id: id}).success(function(data){
+		console.log(data);
+		$scope.data = data;
+		$scope.works = data.works;
+		$scope.banner = data.works[1].profile.photo;
+	});
 
 	$scope.hoverIn = function(id){
 		var pos = data.map(function(x) {return x._id; }).indexOf(id);
